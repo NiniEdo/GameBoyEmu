24e9896130c99afea69b8878745a90293aa348dd
NiniEdo <edoardo.nini@gmail.com>
2024-12-10 10:26:19 +0100

    start of the project

---
+++ GameBoyEmu/Program.cs
@@ -0,0 +1,14 @@
+﻿using GameBoyEmu;
+using RomNamespace;
+
+namespace GameBoyEmu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rom rom = Rom.GetRom();
+            rom.loadFromCartrige();
+        }
+    }
+}
---
+++ GameBoyEmu/rom/Rom.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace RomNamespace
+{
+    internal class Rom
+    {
+        private static Rom _rom = new Rom();
+        private byte[]? romDump;
+        private Rom() { }
+        public static Rom GetRom()
+        {
+            return _rom;
+        }
+
+        public void loadFromCartrige()
+        {
+            string directoryPath = @"..\..\..";
+            String[] files = Directory.GetFiles(directoryPath, "*.gb");
+            String? bgFile = files.FirstOrDefault();
+
+            if (bgFile == null)
+            {
+                Console.WriteLine("file not found");
+                return;
+            }
+
+            romDump = File.ReadAllBytes(bgFile);
+            Console.WriteLine($"{BitConverter.ToString(romDump)}");
+        }
+        public void validateRom()
+        {
+
+        }
+    }
+}
