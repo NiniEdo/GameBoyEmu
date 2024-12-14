0286d1f7b4638b39d6458afb124de5bcf0b84007
NiniEdo <edoardo.nini@gmail.com>
2024-12-14 12:01:00 +0100

    Aggiunte strutture e istruzioni, migliorata gestione CPU
    - Aggiunta importazione di `System.Collections` in `cpu.cs`
    - Rinominata variabile `logger` in `_logger` in `cpu.cs`
    - Aggiunta struttura `Flags` in `cpu.cs` per gestione flag
    - Rimossa gestione eccezioni e inizializzazione `_romDump` in `Cpu`
    - Aggiunta istruzione "LD r16, imm16" in `_instructionSetBlockZero`
    - Corretto calcolo istruzioni nel metodo `decode` in `cpu.cs`
    - Aggiunto file `Memory.cs` con classe `Memory` per gestione ROM

--- GameBoyEmu/cpu/cpu.cs
+++ GameBoyEmu/cpu/cpu.cs
@@ -11,14 +11,14 @@ using System.Reflection.Emit;
 using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Net.NetworkInformation;
+using System.Collections;
 
 namespace GameBoyEmu.CpuNamespace
 {
     internal class Cpu
     {
-        private Logger logger = LogManager.GetCurrentClassLogger();
+        private Logger _logger = LogManager.GetCurrentClassLogger();
         private static Cpu _cpu = new Cpu();
-        private byte[] _romDump = Array.Empty<byte>();
 
         struct Instruction
         {
@@ -32,43 +32,58 @@ namespace GameBoyEmu.CpuNamespace
 
         }
 
-        private static readonly Dictionary<byte, Instruction> _instructionSetBlockZero = new Dictionary<byte, Instruction>
+        struct Flags
         {
-            //last 4 bits -> Instruction
-            { 0b0000, new Instruction("NOP", () => { return; }) },
-            { 0b0001, new Instruction("LD r16, imm16", () => { /* LD r16, imm16 implementation */ }) },
-            //{ 0x02, new Instruction("LD (BC), A", () => { /* LD (BC), A implementation */ }) },
-        };
+            private static byte _F = 0b0000_0000;
 
-        private static readonly Dictionary<byte, Dictionary<byte, Instruction>> _instructionSetBlocks = new Dictionary<byte, Dictionary<byte, Instruction>>
-        {
-            { 0b00,  _instructionSetBlockZero},
-            //{ }
-        };
-
-        private Cpu()
-        {
-            try
+            public static void setZeroFlag(bool value)
             {
-                _romDump = _rom.loadFromCartridge();
+                int intValue = value ? 1 : 0;
+                _F = (byte)((_F & 0b0111_1111) | (intValue << 7));
             }
-            catch (CartridgeException CAex)
+            public static void setSubtractionFlag(bool value)
             {
-                logger.Fatal("Error: " + CAex.Message);
+                int intValue = value ? 1 : 0;
+                _F = (byte)((_F & 0b1011_1111) | (intValue << 6));
+            }
+            public static void setHalfCarryFlag(bool value)
+            {
+                int intValue = value ? 1 : 0;
+                _F = (byte)((_F & 0b1101_1111) | (intValue << 5));
+            }
+            public static void setCarryFlag(bool value)
+            {
+                int intValue = value ? 1 : 0;
+                _F = (byte)((_F & 0b1110_1111) | (intValue << 4));
+            }
+
+            public static byte getZeroFlag()
+            {
+                return (byte)((_F & 0b1000_0000) >> 7);
+            }
+            public static byte getSubtractionFlag()
+            {
+                return (byte)((_F & 0b0100_0000) >> 6);
+            }
+            public static byte getHalfCarryFlag()
+            {
+                return (byte)((_F & 0b0010_0000) >> 5);
+            }
+            public static byte getCarryFlag()
+            {
+                return (byte)((_F & 0b0001_0000) >> 4);
             }
         }
 
-        public static Cpu GetCpu()
-        {
-            return _cpu;
-        }
 
-        private static byte[] _AF = new byte[2];
+        //8 bits
+        private static byte _A;
+        //16 bits
         private static byte[] _BC = new byte[2]; //000
         private static byte[] _DE = new byte[2]; //001
         private static byte[] _HL = new byte[2]; //010
         private static byte[] _SP = new byte[2]; //011 
-        private static byte[] _PC = new byte[] { 0x00, 0x00 };
+        private static byte[] _PC = new byte[2] { 0x00, 0x00 };
 
         private static readonly Dictionary<byte, byte[]> _16bitsRegistries = new Dictionary<byte, byte[]>
         {
@@ -80,7 +95,44 @@ namespace GameBoyEmu.CpuNamespace
 
         byte _instructionRegister;
 
-        Rom _rom = Rom.GetRom();
+
+        private static readonly Dictionary<byte, Instruction> _instructionSetBlockZero = new Dictionary<byte, Instruction>
+        {
+            //last 4 bits -> Instruction
+            { 0b0000, new Instruction("NOP", () => { return; }) },
+            { 0b0001, new Instruction("LD r16, imm16", () =>
+                {
+                    // Fetch the next two bytes for the immediate 16-bit value
+                    byte lowByte = _cpu.fetch();
+                    byte highByte = _cpu.fetch();
+
+                    byte registerCode = (byte)((_cpu._instructionRegister & 0b0011_0000) >> 4);
+                    if (_16bitsRegistries.TryGetValue(registerCode, out byte[]? register))
+                    {
+                        register[0] = highByte;
+                        register[1] = lowByte;
+                    }
+                    else
+                    {
+                        _cpu._logger.Fatal("Invalid 16-bit register code.");
+                    }
+                })
+            },
+            //{ 0x02, new Instruction("LD (BC), A", () => { /* LD (BC), A implementation */ }) },
+        };
+
+        private static readonly Dictionary<byte, Dictionary<byte, Instruction>> _instructionSetBlocks = new Dictionary<byte, Dictionary<byte, Instruction>>
+        {
+            { 0b00,  _instructionSetBlockZero},
+        };
+
+        private Cpu()
+        {}
+
+        public static Cpu GetCpu()
+        {
+            return _cpu;
+        }
 
         byte fetch()
         {
@@ -88,7 +140,7 @@ namespace GameBoyEmu.CpuNamespace
 
             if (pcValue >= _romDump.Length)
             {
-                logger.Fatal("Attempted to fetch beyond ROM boundaries.");
+                _logger.Fatal("Attempted to fetch beyond ROM boundaries.");
                 throw new IndexOutOfRangeException("Program Counter exceeded ROM boundaries.");
             }
 
@@ -108,35 +160,35 @@ namespace GameBoyEmu.CpuNamespace
             try
             {
                 //here it will bind the opcode with the parameter fetching it and executing the code
-                byte block = (byte)(_instructionRegister & (0b1100_0000 >> 7)); // read the instruction block
+                byte block = (byte)(_instructionRegister & (0b1100_0000)); // read the instruction block
                 Dictionary<byte, Instruction> _instructionSetOfCurrentBlock = _instructionSetBlocks[block];
 
-                byte lastFourByteOfinstruction = (byte)(_instructionRegister & (0b0011_0000 >> 3)); // read the instruction
+                byte lastFourByteOfinstruction = (byte)(_instructionRegister & (0b0011_0000 >> 4)); // read the instruction
                 instruction = _instructionSetOfCurrentBlock[lastFourByteOfinstruction];
-                logger.Debug($"Instruction Fetched: {instruction.Name}");
+                _logger.Debug($"Instruction Fetched: {instruction.Name}");
             }
             catch (Exception e)
             {
-                logger.Fatal($"Error while decoding the instriction {e.Message}");
+                _logger.Error($"Error while decoding the instriction {e.Message}");
                 return;
             }
 
             //Execute the intruction
             instruction.Execute();
-
         }
 
         public void execute()
         {
             if (_romDump.Length == 0)
             {
-                logger.Fatal("ROM dump is null. Cannot fetch instructions.");
+                _logger.Fatal("ROM dump is null. Cannot fetch instructions.");
                 return;
             }
 
-            ushort pcValue = (ushort)((_PC[0] << 8) | _PC[1]);
+            //_romDump = new byte[] { 0x01, 0x34, 0x12, 0xFF, 0xA0, 0x5B, 0x9C, 0x00 };
 
-            while (pcValue < _romDump.Length - 1)
+            ushort pcValue = (ushort)((_PC[0] << 8) | _PC[1]);
+            while (pcValue < _romDump.Length)
             {
                 byte data = fetch();
                 _instructionRegister = data;
---
+++ GameBoyEmu/memory/Memory.cs
@@ -0,0 +1,51 @@
+﻿using GameBoyEmu.CpuNamespace;
+using GameBoyEmu.Exceptions;
+using GameBoyEmu.RomNamespace;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBoyEmu.memory
+{
+    internal class Memory
+    {
+        private Logger _logger = LogManager.GetCurrentClassLogger();
+        private static Memory _memory = new Memory();
+        private byte[] _memoryMap = new byte[0xFFFF];
+
+        private byte[] _romDump = Array.Empty<byte>();
+        Rom _rom = Rom.GetRom();
+
+
+        public static Memory GetMemory()
+        {
+            return _memory;
+        }
+
+        private Memory()
+        {
+            try
+            {
+                _romDump = _rom.loadFromCartridge();
+            }
+            catch (CartridgeException CAex)
+            {
+                _logger.Fatal("Error: " + CAex.Message);
+            }
+            setRomBankZero();
+        }
+
+        public byte[] MemoryMap { get => _memoryMap; set => _memoryMap = value; }
+
+        private void setRomBankZero()
+        {
+            for (int i = 0; i < 0x3FFF; i++)
+            {
+                _memoryMap[i] = _romDump[i];
+            }
+        }
+    }
+}
